bd125206ac3f72ff3ab47d61f9a534ac50728e45
rollingcoco <79988832+rollingcoco@users.noreply.github.com>
2023-06-03 15:59:24 +0800

    Create dailyvolume.py

---
+++ dailyvolume.py
@@ -0,0 +1,30 @@
+import pysnowball as ball
+
+with open('/SZ(or SH).txt','r',encoding='utf-8') as f:                    #create a folder to record data
+    alldata=f.read()
+    print(alldata)
+    b=all.split()
+    c = []
+    for x in b:
+        ball.set_token('xqat=bbcf3abaeecc14b646ce7bfb68cffd22094e48ae;')  # taken of snowball
+        aa = ball.quotec('SZ' + x)
+        qudata = aa['data']
+        aimdic = qudata[0]
+        c.append(aimdic)
+
+
+with open('RESZ(or RZSH).txt','w',encoding='utf-8') as g:                 #
+    for d in c:                                                           #
+        for xx, y in d.items():
+
+            if xx == 'volume':
+                g.write(str(y) + '\n')
+
+
+
+
+
+
+
+
+
